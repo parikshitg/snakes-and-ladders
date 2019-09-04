d1e89943d5a3e953db65bc479430e8be402aaeef
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-09-04 14:19:40 +0530

    Initial Commit

---
+++ snakes-and-ladders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  snakes-and-ladders
+//
+//  Created by Parikshit Gothwal on 04/09/19.
+//  Copyright © 2019 Urantia Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
