41b172c791ac4f725fe39cd174e8e99db1c529ee
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-09-05 15:44:05 +0530

    dice images changes

--- snakes-and-ladders/ViewController.swift
+++ snakes-and-ladders/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerOneDice: UIImageView!
+    @IBOutlet weak var playerTwoDice: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //gesture recognizer for dice images
+        let playerOneDiceRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.diceOneImageChange))
+        let playerTwoDiceRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.diceTwoImageChange))
+        
+        //adding gesture recognizer to images
+        playerOneDice.addGestureRecognizer(playerOneDiceRecognizer)
+        playerTwoDice.addGestureRecognizer(playerTwoDiceRecognizer)
+        
+        //enabling images for user interaction
+        playerOneDice.isUserInteractionEnabled = true
+        playerTwoDice.isUserInteractionEnabled = true
+        
     }
 
-
+    //changes dice one image 1-6
+    @objc func diceOneImageChange(){
+        
+        let randomNumber = Int(arc4random_uniform(UInt32(6)))
+        playerOneDice.image = UIImage(named: "\(randomNumber+1)dice")
+    }
+    
+    //changes dice two image 1-6
+    @objc func diceTwoImageChange(){
+        
+        let randomNumber = Int(arc4random_uniform(UInt32(6)))
+        playerTwoDice.image = UIImage(named: "\(randomNumber+1)dice")
+    }
+    
 }
 
