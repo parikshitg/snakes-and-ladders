2d67f9641c6a4163bf2f8c75889a2bb4225e8c68
Parikshit Gothwal <parikshitgothwal14@gmail.com>
2019-09-05 21:03:37 +0530

    color pieces images added

--- snakes-and-ladders/ViewController.swift
+++ snakes-and-ladders/ViewController.swift
@@ -12,6 +12,427 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var playerOneDice: UIImageView!
     @IBOutlet weak var playerTwoDice: UIImageView!
+    @IBOutlet weak var arrow1: UIImageView!
+    @IBOutlet weak var arrow2: UIImageView!
+    
+    
+    //red piece 1-100
+    @IBOutlet weak var red1: UIImageView!
+    @IBOutlet weak var red2: UIImageView!
+    @IBOutlet weak var red3: UIImageView!
+    @IBOutlet weak var red4: UIImageView!
+    @IBOutlet weak var red5: UIImageView!
+    @IBOutlet weak var red6: UIImageView!
+    @IBOutlet weak var red7: UIImageView!
+    @IBOutlet weak var red8: UIImageView!
+    @IBOutlet weak var red9: UIImageView!
+    @IBOutlet weak var red10: UIImageView!
+    @IBOutlet weak var red11: UIImageView!
+    @IBOutlet weak var red12: UIImageView!
+    @IBOutlet weak var red13: UIImageView!
+    @IBOutlet weak var red14: UIImageView!
+    @IBOutlet weak var red15: UIImageView!
+    @IBOutlet weak var red16: UIImageView!
+    @IBOutlet weak var red17: UIImageView!
+    @IBOutlet weak var red18: UIImageView!
+    @IBOutlet weak var red19: UIImageView!
+    @IBOutlet weak var red20: UIImageView!
+    @IBOutlet weak var red21: UIImageView!
+    @IBOutlet weak var red22: UIImageView!
+    @IBOutlet weak var red23: UIImageView!
+    @IBOutlet weak var red24: UIImageView!
+    @IBOutlet weak var red25: UIImageView!
+    @IBOutlet weak var red26: UIImageView!
+    @IBOutlet weak var red27: UIImageView!
+    @IBOutlet weak var red28: UIImageView!
+    @IBOutlet weak var red29: UIImageView!
+    @IBOutlet weak var red30: UIImageView!
+    @IBOutlet weak var red31: UIImageView!
+    @IBOutlet weak var red32: UIImageView!
+    @IBOutlet weak var red33: UIImageView!
+    @IBOutlet weak var red34: UIImageView!
+    @IBOutlet weak var red35: UIImageView!
+    @IBOutlet weak var red36: UIImageView!
+    @IBOutlet weak var red37: UIImageView!
+    @IBOutlet weak var red38: UIImageView!
+    @IBOutlet weak var red39: UIImageView!
+    @IBOutlet weak var red40: UIImageView!
+    @IBOutlet weak var red41: UIImageView!
+    @IBOutlet weak var red42: UIImageView!
+    @IBOutlet weak var red43: UIImageView!
+    @IBOutlet weak var red44: UIImageView!
+    @IBOutlet weak var red45: UIImageView!
+    @IBOutlet weak var red46: UIImageView!
+    @IBOutlet weak var red47: UIImageView!
+    @IBOutlet weak var red48: UIImageView!
+    @IBOutlet weak var red49: UIImageView!
+    @IBOutlet weak var red50: UIImageView!
+    @IBOutlet weak var red51: UIImageView!
+    @IBOutlet weak var red52: UIImageView!
+    @IBOutlet weak var red53: UIImageView!
+    @IBOutlet weak var red54: UIImageView!
+    @IBOutlet weak var red55: UIImageView!
+    @IBOutlet weak var red56: UIImageView!
+    @IBOutlet weak var red57: UIImageView!
+    @IBOutlet weak var red58: UIImageView!
+    @IBOutlet weak var red59: UIImageView!
+    @IBOutlet weak var red60: UIImageView!
+    @IBOutlet weak var red61: UIImageView!
+    @IBOutlet weak var red62: UIImageView!
+    @IBOutlet weak var red63: UIImageView!
+    @IBOutlet weak var red64: UIImageView!
+    @IBOutlet weak var red65: UIImageView!
+    @IBOutlet weak var red66: UIImageView!
+    @IBOutlet weak var red67: UIImageView!
+    @IBOutlet weak var red68: UIImageView!
+    @IBOutlet weak var red69: UIImageView!
+    @IBOutlet weak var red70: UIImageView!
+    @IBOutlet weak var red71: UIImageView!
+    @IBOutlet weak var red72: UIImageView!
+    @IBOutlet weak var red73: UIImageView!
+    @IBOutlet weak var red74: UIImageView!
+    @IBOutlet weak var red75: UIImageView!
+    @IBOutlet weak var red76: UIImageView!
+    @IBOutlet weak var red77: UIImageView!
+    @IBOutlet weak var red78: UIImageView!
+    @IBOutlet weak var red79: UIImageView!
+    @IBOutlet weak var red80: UIImageView!
+    @IBOutlet weak var red81: UIImageView!
+    @IBOutlet weak var red82: UIImageView!
+    @IBOutlet weak var red83: UIImageView!
+    @IBOutlet weak var red84: UIImageView!
+    @IBOutlet weak var red85: UIImageView!
+    @IBOutlet weak var red86: UIImageView!
+    @IBOutlet weak var red87: UIImageView!
+    @IBOutlet weak var red88: UIImageView!
+    @IBOutlet weak var red89: UIImageView!
+    @IBOutlet weak var red90: UIImageView!
+    @IBOutlet weak var red91: UIImageView!
+    @IBOutlet weak var red92: UIImageView!
+    @IBOutlet weak var red93: UIImageView!
+    @IBOutlet weak var red94: UIImageView!
+    @IBOutlet weak var red95: UIImageView!
+    @IBOutlet weak var red96: UIImageView!
+    @IBOutlet weak var red97: UIImageView!
+    @IBOutlet weak var red98: UIImageView!
+    @IBOutlet weak var red99: UIImageView!
+    @IBOutlet weak var red100: UIImageView!
+    
+    
+    
+    
+    //yellow piece 1-100
+    @IBOutlet weak var yellow1: UIImageView!
+    @IBOutlet weak var yellow2: UIImageView!
+    @IBOutlet weak var yellow3: UIImageView!
+    @IBOutlet weak var yellow4: UIImageView!
+    @IBOutlet weak var yellow5: UIImageView!
+    @IBOutlet weak var yellow6: UIImageView!
+    @IBOutlet weak var yellow7: UIImageView!
+    @IBOutlet weak var yellow8: UIImageView!
+    @IBOutlet weak var yellow9: UIImageView!
+    @IBOutlet weak var yellow10: UIImageView!
+    @IBOutlet weak var yellow11: UIImageView!
+    @IBOutlet weak var yellow12: UIImageView!
+    @IBOutlet weak var yellow13: UIImageView!
+    @IBOutlet weak var yellow14: UIImageView!
+    @IBOutlet weak var yellow15: UIImageView!
+    @IBOutlet weak var yellow16: UIImageView!
+    @IBOutlet weak var yellow17: UIImageView!
+    @IBOutlet weak var yellow18: UIImageView!
+    @IBOutlet weak var yellow19: UIImageView!
+    @IBOutlet weak var yellow20: UIImageView!
+    @IBOutlet weak var yellow21: UIImageView!
+    @IBOutlet weak var yellow22: UIImageView!
+    @IBOutlet weak var yellow23: UIImageView!
+    @IBOutlet weak var yellow24: UIImageView!
+    @IBOutlet weak var yellow25: UIImageView!
+    @IBOutlet weak var yellow26: UIImageView!
+    @IBOutlet weak var yellow27: UIImageView!
+    @IBOutlet weak var yellow28: UIImageView!
+    @IBOutlet weak var yellow29: UIImageView!
+    @IBOutlet weak var yellow30: UIImageView!
+    @IBOutlet weak var yellow31: UIImageView!
+    @IBOutlet weak var yellow32: UIImageView!
+    @IBOutlet weak var yellow33: UIImageView!
+    @IBOutlet weak var yellow34: UIImageView!
+    @IBOutlet weak var yellow35: UIImageView!
+    @IBOutlet weak var yellow36: UIImageView!
+    @IBOutlet weak var yellow37: UIImageView!
+    @IBOutlet weak var yellow38: UIImageView!
+    @IBOutlet weak var yellow39: UIImageView!
+    @IBOutlet weak var yellow40: UIImageView!
+    @IBOutlet weak var yellow41: UIImageView!
+    @IBOutlet weak var yellow42: UIImageView!
+    @IBOutlet weak var yellow43: UIImageView!
+    @IBOutlet weak var yellow44: UIImageView!
+    @IBOutlet weak var yellow45: UIImageView!
+    @IBOutlet weak var yellow46: UIImageView!
+    @IBOutlet weak var yellow47: UIImageView!
+    @IBOutlet weak var yellow48: UIImageView!
+    @IBOutlet weak var yellow49: UIImageView!
+    @IBOutlet weak var yellow50: UIImageView!
+    @IBOutlet weak var yellow51: UIImageView!
+    @IBOutlet weak var yellow52: UIImageView!
+    @IBOutlet weak var yellow53: UIImageView!
+    @IBOutlet weak var yellow54: UIImageView!
+    @IBOutlet weak var yellow55: UIImageView!
+    @IBOutlet weak var yellow56: UIImageView!
+    @IBOutlet weak var yellow57: UIImageView!
+    @IBOutlet weak var yellow58: UIImageView!
+    @IBOutlet weak var yellow59: UIImageView!
+    @IBOutlet weak var yellow60: UIImageView!
+    @IBOutlet weak var yellow61: UIImageView!
+    @IBOutlet weak var yellow62: UIImageView!
+    @IBOutlet weak var yellow63: UIImageView!
+    @IBOutlet weak var yellow64: UIImageView!
+    @IBOutlet weak var yellow65: UIImageView!
+    @IBOutlet weak var yellow66: UIImageView!
+    @IBOutlet weak var yellow67: UIImageView!
+    @IBOutlet weak var yellow68: UIImageView!
+    @IBOutlet weak var yellow69: UIImageView!
+    @IBOutlet weak var yellow70: UIImageView!
+    @IBOutlet weak var yellow71: UIImageView!
+    @IBOutlet weak var yellow72: UIImageView!
+    @IBOutlet weak var yellow73: UIImageView!
+    @IBOutlet weak var yellow74: UIImageView!
+    @IBOutlet weak var yellow75: UIImageView!
+    @IBOutlet weak var yellow76: UIImageView!
+    @IBOutlet weak var yellow77: UIImageView!
+    @IBOutlet weak var yellow78: UIImageView!
+    @IBOutlet weak var yellow79: UIImageView!
+    @IBOutlet weak var yellow80: UIImageView!
+    @IBOutlet weak var yellow81: UIImageView!
+    @IBOutlet weak var yellow82: UIImageView!
+    @IBOutlet weak var yellow83: UIImageView!
+    @IBOutlet weak var yellow84: UIImageView!
+    @IBOutlet weak var yellow85: UIImageView!
+    @IBOutlet weak var yellow86: UIImageView!
+    @IBOutlet weak var yellow87: UIImageView!
+    @IBOutlet weak var yellow88: UIImageView!
+    @IBOutlet weak var yellow89: UIImageView!
+    @IBOutlet weak var yellow90: UIImageView!
+    @IBOutlet weak var yellow91: UIImageView!
+    @IBOutlet weak var yellow92: UIImageView!
+    @IBOutlet weak var yellow93: UIImageView!
+    @IBOutlet weak var yellow94: UIImageView!
+    @IBOutlet weak var yellow95: UIImageView!
+    @IBOutlet weak var yellow96: UIImageView!
+    @IBOutlet weak var yellow97: UIImageView!
+    @IBOutlet weak var yellow98: UIImageView!
+    @IBOutlet weak var yellow99: UIImageView!
+    @IBOutlet weak var yellow100: UIImageView!
+    
+    //blue pieces 1-100
+    @IBOutlet weak var blue1: UIImageView!
+    @IBOutlet weak var blue2: UIImageView!
+    @IBOutlet weak var blue3: UIImageView!
+    @IBOutlet weak var blue4: UIImageView!
+    @IBOutlet weak var blue5: UIImageView!
+    @IBOutlet weak var blue6: UIImageView!
+    @IBOutlet weak var blue7: UIImageView!
+    @IBOutlet weak var blue8: UIImageView!
+    @IBOutlet weak var blue9: UIImageView!
+    @IBOutlet weak var blue10: UIImageView!
+    @IBOutlet weak var blue11: UIImageView!
+    @IBOutlet weak var blue12: UIImageView!
+    @IBOutlet weak var blue13: UIImageView!
+    @IBOutlet weak var blue14: UIImageView!
+    @IBOutlet weak var blue15: UIImageView!
+    @IBOutlet weak var blue16: UIImageView!
+    @IBOutlet weak var blue17: UIImageView!
+    @IBOutlet weak var blue18: UIImageView!
+    @IBOutlet weak var blue19: UIImageView!
+    @IBOutlet weak var blue20: UIImageView!
+    @IBOutlet weak var blue21: UIImageView!
+    @IBOutlet weak var blue22: UIImageView!
+    @IBOutlet weak var blue23: UIImageView!
+    @IBOutlet weak var blue24: UIImageView!
+    @IBOutlet weak var blue25: UIImageView!
+    @IBOutlet weak var blue26: UIImageView!
+    @IBOutlet weak var blue27: UIImageView!
+    @IBOutlet weak var blue28: UIImageView!
+    @IBOutlet weak var blue29: UIImageView!
+    @IBOutlet weak var blue30: UIImageView!
+    @IBOutlet weak var blue31: UIImageView!
+    @IBOutlet weak var blue32: UIImageView!
+    @IBOutlet weak var blue33: UIImageView!
+    @IBOutlet weak var blue34: UIImageView!
+    @IBOutlet weak var blue35: UIImageView!
+    @IBOutlet weak var blue36: UIImageView!
+    @IBOutlet weak var blue37: UIImageView!
+    @IBOutlet weak var blue38: UIImageView!
+    @IBOutlet weak var blue39: UIImageView!
+    @IBOutlet weak var blue40: UIImageView!
+    @IBOutlet weak var blue41: UIImageView!
+    @IBOutlet weak var blue42: UIImageView!
+    @IBOutlet weak var blue43: UIImageView!
+    @IBOutlet weak var blue44: UIImageView!
+    @IBOutlet weak var blue45: UIImageView!
+    @IBOutlet weak var blue46: UIImageView!
+    @IBOutlet weak var blue47: UIImageView!
+    @IBOutlet weak var blue48: UIImageView!
+    @IBOutlet weak var blue49: UIImageView!
+    @IBOutlet weak var blue50: UIImageView!
+    @IBOutlet weak var blue51: UIImageView!
+    @IBOutlet weak var blue52: UIImageView!
+    @IBOutlet weak var blue53: UIImageView!
+    @IBOutlet weak var blue54: UIImageView!
+    @IBOutlet weak var blue55: UIImageView!
+    @IBOutlet weak var blue56: UIImageView!
+    @IBOutlet weak var blue57: UIImageView!
+    @IBOutlet weak var blue58: UIImageView!
+    @IBOutlet weak var blue59: UIImageView!
+    @IBOutlet weak var blue60: UIImageView!
+    @IBOutlet weak var blue61: UIImageView!
+    @IBOutlet weak var blue62: UIImageView!
+    @IBOutlet weak var blue63: UIImageView!
+    @IBOutlet weak var blue64: UIImageView!
+    @IBOutlet weak var blue65: UIImageView!
+    @IBOutlet weak var blue66: UIImageView!
+    @IBOutlet weak var blue67: UIImageView!
+    @IBOutlet weak var blue68: UIImageView!
+    @IBOutlet weak var blue69: UIImageView!
+    @IBOutlet weak var blue70: UIImageView!
+    @IBOutlet weak var blue71: UIImageView!
+    @IBOutlet weak var blue72: UIImageView!
+    @IBOutlet weak var blue73: UIImageView!
+    @IBOutlet weak var blue74: UIImageView!
+    @IBOutlet weak var blue75: UIImageView!
+    @IBOutlet weak var blue76: UIImageView!
+    @IBOutlet weak var blue77: UIImageView!
+    @IBOutlet weak var blue78: UIImageView!
+    @IBOutlet weak var blue79: UIImageView!
+    @IBOutlet weak var blue80: UIImageView!
+    @IBOutlet weak var blue81: UIImageView!
+    @IBOutlet weak var blue82: UIImageView!
+    @IBOutlet weak var blue83: UIImageView!
+    @IBOutlet weak var blue84: UIImageView!
+    @IBOutlet weak var blue85: UIImageView!
+    @IBOutlet weak var blue86: UIImageView!
+    @IBOutlet weak var blue87: UIImageView!
+    @IBOutlet weak var blue88: UIImageView!
+    @IBOutlet weak var blue89: UIImageView!
+    @IBOutlet weak var blue90: UIImageView!
+    @IBOutlet weak var blue91: UIImageView!
+    @IBOutlet weak var blue92: UIImageView!
+    @IBOutlet weak var blue93: UIImageView!
+    @IBOutlet weak var blue94: UIImageView!
+    @IBOutlet weak var blue95: UIImageView!
+    @IBOutlet weak var blue96: UIImageView!
+    @IBOutlet weak var blue97: UIImageView!
+    @IBOutlet weak var blue98: UIImageView!
+    @IBOutlet weak var blue99: UIImageView!
+    @IBOutlet weak var blue100: UIImageView!
+    
+    //green pieces 1-100
+    @IBOutlet weak var green1: UIImageView!
+    @IBOutlet weak var green2: UIImageView!
+    @IBOutlet weak var green3: UIImageView!
+    @IBOutlet weak var green4: UIImageView!
+    @IBOutlet weak var green5: UIImageView!
+    @IBOutlet weak var green6: UIImageView!
+    @IBOutlet weak var green7: UIImageView!
+    @IBOutlet weak var green8: UIImageView!
+    @IBOutlet weak var green9: UIImageView!
+    @IBOutlet weak var green10: UIImageView!
+    @IBOutlet weak var green11: UIImageView!
+    @IBOutlet weak var green12: UIImageView!
+    @IBOutlet weak var green13: UIImageView!
+    @IBOutlet weak var green14: UIImageView!
+    @IBOutlet weak var green15: UIImageView!
+    @IBOutlet weak var green16: UIImageView!
+    @IBOutlet weak var green17: UIImageView!
+    @IBOutlet weak var green18: UIImageView!
+    @IBOutlet weak var green19: UIImageView!
+    @IBOutlet weak var green20: UIImageView!
+    @IBOutlet weak var green21: UIImageView!
+    @IBOutlet weak var green22: UIImageView!
+    @IBOutlet weak var green23: UIImageView!
+    @IBOutlet weak var green24: UIImageView!
+    @IBOutlet weak var green25: UIImageView!
+    @IBOutlet weak var green26: UIImageView!
+    @IBOutlet weak var green27: UIImageView!
+    @IBOutlet weak var green28: UIImageView!
+    @IBOutlet weak var green29: UIImageView!
+    @IBOutlet weak var green30: UIImageView!
+    @IBOutlet weak var green31: UIImageView!
+    @IBOutlet weak var green32: UIImageView!
+    @IBOutlet weak var green33: UIImageView!
+    @IBOutlet weak var green34: UIImageView!
+    @IBOutlet weak var green35: UIImageView!
+    @IBOutlet weak var green36: UIImageView!
+    @IBOutlet weak var green37: UIImageView!
+    @IBOutlet weak var green38: UIImageView!
+    @IBOutlet weak var green39: UIImageView!
+    @IBOutlet weak var green40: UIImageView!
+    @IBOutlet weak var green41: UIImageView!
+    @IBOutlet weak var green42: UIImageView!
+    @IBOutlet weak var green43: UIImageView!
+    @IBOutlet weak var green44: UIImageView!
+    @IBOutlet weak var green45: UIImageView!
+    @IBOutlet weak var green46: UIImageView!
+    @IBOutlet weak var green47: UIImageView!
+    @IBOutlet weak var green48: UIImageView!
+    @IBOutlet weak var green49: UIImageView!
+    @IBOutlet weak var green50: UIImageView!
+    @IBOutlet weak var green51: UIImageView!
+    @IBOutlet weak var green52: UIImageView!
+    @IBOutlet weak var green53: UIImageView!
+    @IBOutlet weak var green54: UIImageView!
+    @IBOutlet weak var green55: UIImageView!
+    @IBOutlet weak var green56: UIImageView!
+    @IBOutlet weak var green57: UIImageView!
+    @IBOutlet weak var green58: UIImageView!
+    @IBOutlet weak var green59: UIImageView!
+    @IBOutlet weak var green60: UIImageView!
+    @IBOutlet weak var green61: UIImageView!
+    @IBOutlet weak var green62: UIImageView!
+    @IBOutlet weak var green63: UIImageView!
+    @IBOutlet weak var green64: UIImageView!
+    @IBOutlet weak var green65: UIImageView!
+    @IBOutlet weak var green66: UIImageView!
+    @IBOutlet weak var green67: UIImageView!
+    @IBOutlet weak var green68: UIImageView!
+    @IBOutlet weak var green69: UIImageView!
+    @IBOutlet weak var green70: UIImageView!
+    @IBOutlet weak var green71: UIImageView!
+    @IBOutlet weak var green72: UIImageView!
+    @IBOutlet weak var green73: UIImageView!
+    @IBOutlet weak var green74: UIImageView!
+    @IBOutlet weak var green75: UIImageView!
+    @IBOutlet weak var green76: UIImageView!
+    @IBOutlet weak var green77: UIImageView!
+    @IBOutlet weak var green78: UIImageView!
+    @IBOutlet weak var green79: UIImageView!
+    @IBOutlet weak var green80: UIImageView!
+    @IBOutlet weak var green81: UIImageView!
+    @IBOutlet weak var green82: UIImageView!
+    @IBOutlet weak var green83: UIImageView!
+    @IBOutlet weak var green84: UIImageView!
+    @IBOutlet weak var green85: UIImageView!
+    @IBOutlet weak var green86: UIImageView!
+    @IBOutlet weak var green87: UIImageView!
+    @IBOutlet weak var green88: UIImageView!
+    @IBOutlet weak var green89: UIImageView!
+    @IBOutlet weak var green90: UIImageView!
+    @IBOutlet weak var green91: UIImageView!
+    @IBOutlet weak var green92: UIImageView!
+    @IBOutlet weak var green93: UIImageView!
+    @IBOutlet weak var green94: UIImageView!
+    @IBOutlet weak var green95: UIImageView!
+    @IBOutlet weak var green96: UIImageView!
+    @IBOutlet weak var green97: UIImageView!
+    @IBOutlet weak var green98: UIImageView!
+    @IBOutlet weak var green99: UIImageView!
+    @IBOutlet weak var green100: UIImageView!
+    
+    
+    
+    
+    
+    
+    
     
     
     override func viewDidLoad() {
@@ -28,7 +449,7 @@ class ViewController: UIViewController {
         //enabling images for user interaction
         playerOneDice.isUserInteractionEnabled = true
         playerTwoDice.isUserInteractionEnabled = true
-        
+
     }
 
     //changes dice one image 1-6
@@ -36,6 +457,7 @@ class ViewController: UIViewController {
         
         let randomNumber = Int(arc4random_uniform(UInt32(6)))
         playerOneDice.image = UIImage(named: "\(randomNumber+1)dice")
+
     }
     
     //changes dice two image 1-6
@@ -43,6 +465,7 @@ class ViewController: UIViewController {
         
         let randomNumber = Int(arc4random_uniform(UInt32(6)))
         playerTwoDice.image = UIImage(named: "\(randomNumber+1)dice")
+
     }
     
 }
